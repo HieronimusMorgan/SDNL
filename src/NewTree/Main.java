39524c961628daf6804ab3a9dbe0e0b99669264d
asus <asus@MORGAN>
2019-06-12 21:12:11 +0700

    Repair delete 2 child

--- src/NewTree/Main.java
+++ src/NewTree/Main.java
@@ -8,13 +8,13 @@ public class Main {
         for (int i = 0; i < data.length; i++) {
             number.insertNode(data[i]);
         }
-        System.out.println("Before Delete : ");
+        System.out.println("Before Delete   : ");
         System.out.print("In Order          : ");
         number.preOrderTransversal();
         System.out.println("");
         number.delete(71);
         number.delete(21);
-        System.out.println("After Delete        : ");
+        System.out.println("After Delete    : ");
         System.out.print("In Order          : ");
         number.preOrderTransversal();
     }
